822945c38ee12df095dba6e694d6ee0cf5527e08
shilash-m <78814577+shilash-m@users.noreply.github.com>
2021-07-03 15:30:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
