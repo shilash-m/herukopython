ef339de5cca90fc8b793f150faaf2aee27d28087
shilash-m <78814577+shilash-m@users.noreply.github.com>
2021-07-03 22:26:08 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from app import app
+from app import application
 from db import db
 
-db.init_app(app)
+db.init_app(application)
 
-@app.before_first_request
+@application.before_first_request
 def create_tables():
     db.create_all()
