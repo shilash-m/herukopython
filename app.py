2c6afc550369062f964405ceba43fff04ebdb97f
shilash-m <78814577+shilash-m@users.noreply.github.com>
2021-07-03 15:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.config['DEBUG'] = True
 
 app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
+app.secret_key = 'jos'
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -32,6 +32,6 @@ if __name__ == '__main__':
     
     @app.before_first_request
     def create_tables():
-    db.create_all()
+        db.create_all()
 
     app.run(port=5000)
